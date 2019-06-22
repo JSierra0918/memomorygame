4cd42c369fd397b279b39b60e43ec3fdddb1ce66
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-06-21 21:18:33 -0400

    Updates

---
+++ precache-manifest.fcd4a939fc9f6fce68faf008f6871067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a3e981f6e640d48740647ce067a2ba",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "b7018574694a368e3bcf",
+    "url": "/memorygame/static/css/main.b4f776f1.chunk.css"
+  },
+  {
+    "revision": "b5d09923a146a52176bf",
+    "url": "/memorygame/static/js/2.b6950a07.chunk.js"
+  },
+  {
+    "revision": "b7018574694a368e3bcf",
+    "url": "/memorygame/static/js/main.a3679469.chunk.js"
+  },
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memorygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
