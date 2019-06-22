d36de322fef708191d4b0e0f1c772d1e7cbad0b9
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-06-21 20:57:31 -0400

    on click can't differentiate from different elements

--- memory-game/src/components/GameContainer.js
+++ memory-game/src/components/GameContainer.js
@@ -5,7 +5,7 @@ import Grid from './Grid';
 import ImageArray from '../Images.json';
 
   //Global Variable
-  let newImageArray = [];
+  let newImageArray = [...ImageArray];
 
 class GameContainer extends React.Component {
     state = {
@@ -15,7 +15,7 @@ class GameContainer extends React.Component {
     }
   
 
-     addScore = () => {
+     addScore = (event) => {
         let currentTopScore = this.state.topScore;
         let newScore = this.state.score;
         newScore++;
@@ -23,29 +23,52 @@ class GameContainer extends React.Component {
         if (newScore >= currentTopScore) {
             currentTopScore = newScore;
         }
+        //check if you you've already clicked on it.
+        if (this.state.clicked === true) {
+            console.log("You've already clicked on it!!!");
+        }
 
+        console.log(event.target);
+ 
         this.setState({
             score:  newScore,
             topScore: currentTopScore,
-            clicked: this.state.clicked = true
+            clicked: true
         })
     }
 
-    //create a random Grid element
-    renderElements = () => {
-         newImageArray = [...ImageArray];
-
-        for (let i = 0; i < ImageArray.length; i++) {
-            let randomNum = Math.floor(Math.random()*ImageArray.length);
-
-            const element =ImageArray[randomNum];
-            let url =process.env.PUBLIC_URL + element.image;
-
-            //This for loop is only running once.
-            console.log(url);
-        return  <Grid image={url} wasClicked={this.addScore} key={element.id}/> 
+     shuffle = (array) => {
+        let counter = array.length;
+    
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            let index = Math.floor(Math.random() * counter);
+    
+            // Decrease counter by 1
+            counter--;
+    
+            // And swap the last element with it
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
         }
-        // return <Grid image={process.env.PUBLIC_URL + "/assets/ace.jpeg"} wasClicked={this.addScore}/> 
+
+        const domArray = array.map((item) => {
+            let url =process.env.PUBLIC_URL + item.image;
+        
+            return  <Grid image={url} wasClicked={this.addScore} key={item.id} dataClick={this.state.clicked} dataId={item.id}/>
+        });    
+        return domArray;
+    }
+
+    reset = () => {
+        // reset the array
+        newImageArray  = [...ImageArray];
+
+        this.setState({
+            score: 0
+        })
     }
 
     render(){
@@ -56,7 +79,7 @@ class GameContainer extends React.Component {
                     <h3>HELLO There!</h3>
                     <div className = "grid-container"> 
                         <Grid image={process.env.PUBLIC_URL + "/assets/ace.jpeg"} wasClicked={this.addScore}/> 
-                        {this.renderElements()} 
+                        {this.shuffle(newImageArray)}
                     </div>
                 </Main>
             </div>
